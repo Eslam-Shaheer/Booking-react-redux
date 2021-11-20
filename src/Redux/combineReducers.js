84aaacd1880f0d94af5b7ef8f76a85e1cbc50e94
Eslam-Shaheer <eslamshaheer@gmail.com>
2021-11-20 20:38:35 +0200

    Image reducer

--- src/Redux/combineReducers.js
+++ src/Redux/combineReducers.js
@@ -1,8 +1,9 @@
 import { combineReducers } from "redux";
 import hotelReducer from "./reducers/hotel";
-
+import imagesReducer from "./reducers/images";
 const rootReducer = combineReducers({
   hotel: hotelReducer,
+  images: imagesReducer,
 });
 
 export default rootReducer;
