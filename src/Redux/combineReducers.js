39453a7a6067936af33f73557b9a776349e1d3d9
Ismail <ismailmohammedraslan@gmail.com>
2021-11-23 02:03:45 +0200

    add navbar

--- src/Redux/combineReducers.js
+++ src/Redux/combineReducers.js
@@ -4,10 +4,6 @@ import hotelReducer from "./reducers/hotel";
 import campGroundReducer from "./reducers/campground";
 import userReducer from "./reducers/user";
 import imagesReducer from "./reducers/images";
-
-
-
-
 const rootReducer = combineReducers({
   hotel: hotelReducer,
   apartment: apartmentReducer,
--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Nav } from "react-bootstrap";
 
 import "./Navbar.css";
 
@@ -10,6 +11,7 @@ export default function Navbar() {
           <a className="navbar-brand font-white fw-bold fs-4" href="#">
             Booking.com
           </a>
+
           <button
             className="navbar-toggler"
             type="button"
@@ -102,6 +104,40 @@ export default function Navbar() {
           </div>
         </div>
       </nav>
+      <div className="navbar navbar-expand-lg bg-primary_color text-white ">
+        <div className="container ">
+          <Nav>
+            <Nav.Link className="text-white NavStyle" href="#home">
+              <svg
+                className="bk-icon -streamline-bed me-1"
+                fill="white"
+                height="24"
+                width="24"
+                viewBox="0 0 24 24"
+                role="presentation"
+                aria-hidden="true"
+                focusable="false"
+              >
+                <path d="M2.75 12h18.5c.69 0 1.25.56 1.25 1.25V18l.75-.75H.75l.75.75v-4.75c0-.69.56-1.25 1.25-1.25zm0-1.5A2.75 2.75 0 0 0 0 13.25V18c0 .414.336.75.75.75h22.5A.75.75 0 0 0 24 18v-4.75a2.75 2.75 0 0 0-2.75-2.75H2.75zM0 18v3a.75.75 0 0 0 1.5 0v-3A.75.75 0 0 0 0 18zm22.5 0v3a.75.75 0 0 0 1.5 0v-3a.75.75 0 0 0-1.5 0zm-.75-6.75V4.5a2.25 2.25 0 0 0-2.25-2.25h-15A2.25 2.25 0 0 0 2.25 4.5v6.75a.75.75 0 0 0 1.5 0V4.5a.75.75 0 0 1 .75-.75h15a.75.75 0 0 1 .75.75v6.75a.75.75 0 0 0 1.5 0zm-13.25-3h7a.25.25 0 0 1 .25.25v2.75l.75-.75h-9l.75.75V8.5a.25.25 0 0 1 .25-.25zm0-1.5A1.75 1.75 0 0 0 6.75 8.5v2.75c0 .414.336.75.75.75h9a.75.75 0 0 0 .75-.75V8.5a1.75 1.75 0 0 0-1.75-1.75h-7z"></path>
+              </svg>{" "}
+              Home
+            </Nav.Link>
+            <Nav.Link className="text-white NavStyle" href="#hotel">
+              Hotel
+            </Nav.Link>
+            <Nav.Link className="text-white NavStyle" href="#apartment">
+              Apartment
+            </Nav.Link>
+            <Nav.Link className="text-white NavStyle" href="#campground">
+              Campground
+            </Nav.Link>
+            <Nav.Link className="text-white NavStyle" href="#community">
+              Community
+            </Nav.Link>
+          </Nav>
+        </div>
+      </div>
     </>
   );
 }
+
