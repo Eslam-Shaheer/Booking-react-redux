74442cea9a6efa8f04b865be2e57362cb8e2465d
Ismail <ismailmohammedraslan@gmail.com>
2021-11-23 02:17:25 +0200

    fixNavbar

--- src/Redux/combineReducers.js
+++ src/Redux/combineReducers.js
@@ -4,14 +4,11 @@ import hotelReducer from "./reducers/hotel";
 import campGroundReducer from "./reducers/campground";
 import userReducer from "./reducers/user";
 import imagesReducer from "./reducers/images";
-<<<<<<< HEAD
-=======
 import propertysFilter from "./reducers/PopsFilter";
 
 
 
 
->>>>>>> 6a8ef6a4953b2696296adc1e714c7608088062b0
 const rootReducer = combineReducers({
   hotel: hotelReducer,
   apartment: apartmentReducer,
