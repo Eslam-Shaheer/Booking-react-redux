ddbe5f6d7169a52061580e46fcbf87b67b31fa40
Eslam-Shaheer <eslamshaheer@gmail.com>
2021-11-24 17:08:40 +0200

    --

--- src/App.js
+++ src/App.js
@@ -5,20 +5,6 @@ import { getHotels } from "./Redux/actions/hotel";
 import { getHotelById } from "./Redux/actions/hotel";
 
 function App() {
-  const upload = (event) => {
-    const formData = new FormData();
-
-    console.log(event);
-  };
-
-  return (
-    <div className="App">
-      <input
-        type="file"
-        name="multiple_images"
-        onChange={(event) => upload(event)}
-      />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 export default App;
