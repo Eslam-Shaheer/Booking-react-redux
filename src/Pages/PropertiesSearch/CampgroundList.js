89908ca8dd591ad277f81c8d431e0c5d78530f5a
Ismail <ismailmohammedraslan@gmail.com>
2021-12-16 12:44:18 +0200

    Search

--- src/Pages/PropertiesSearch/CampgroundList.js
+++ src/Pages/PropertiesSearch/CampgroundList.js
@@ -1,15 +1,15 @@
 import React from "react";
 import CampGrounds from "../../component/Properties/PropertiesCom/CampCom";
 import HotelsCom from "../../component/Properties/PropertiesCom/HotelsCom";
-import SearchCom from "../../component/Properties/Search/SearchCom";
-import { useParams } from "react-router-dom";
+import Search from "../../component/HotelPage/Search/Search";
+ import { useParams } from "react-router-dom";
 export default function CampsSearch() {
   const { country, city } = useParams();
   return (
     <>
-      <div className="container-lg mt-5">
+      <div className="container-lg mt-5 d-flex">
         <div className="row">
-          <SearchCom />
+          <Search />
           <CampGrounds country={country} city={city} />
         </div>
       </div>
